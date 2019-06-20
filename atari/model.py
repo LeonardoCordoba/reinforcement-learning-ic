3a6f5a46ed7eba12d6842a20027458ae3191b4ab
Usuario <cordoba.leonardoignacio@gmail.com>
2019-06-20 19:24:51 -0300

    add model

---
+++ atari/model.py
@@ -0,0 +1,92 @@
+from keras.optimizers import RMSprop, Adam
+from keras.models import Sequential
+from keras.layers import Conv2D, Flatten, Dense
+
+
+class CNNModel:
+    """
+    Keras wrapper to build CNN to play Atari. Based on different papers.
+    """
+
+    def __init__(self):
+        self.model = Sequential()
+
+    def set_model_params(self, parameters):
+        """Method used to pass parameters to CNNModel and build the NN.
+
+        Parameters
+        ----------
+        parameters : dict
+            Dict must have cnn and dense keys. Each key must have a list of
+            dicts. Each dict in the list represents the parameters to build a
+            layer in the model.
+            For example:
+
+
+
+        Returns
+        -------
+        type
+            Description of returned object.
+
+        """
+        assert "cnn" in parameters.keys(), """parameters must have a cnn key
+        with a list of Conv2D dicts hyperparameters"""
+
+        assert "dense" in parameters.keys(), """parameters must have a dense key
+        with a list of dicts Dense hyperparameters"""
+
+        assert "compiler" in parameters.keys(), """parameters must have a
+        compiler key with a dict with loss, optimizer and metrcs
+        hyperparameters"""
+
+        cnn_layers = parameters["cnn"]
+        dense_layers = parameters["dense"]
+        compiler = parameters["compiler"]
+
+        # First, add CNN layers
+        for cnn_hp in cnn_layers:
+            # TODO: unpack dict, not working this way
+            self.model.add(Conv2D(cnn_hp))
+
+        # Second, flatten
+        self.model.add(Flatten())
+
+        # Third, add dense layers
+        for dense_hp in dense_layers:
+            self.model.add(Dense(dense_hp))
+
+        # Last, compile
+        self.model.compile(compiler)
+
+        return self.model.summary()
+
+cnn = CNNModel()
+
+conv_1 = {"filters": 32, "kernel_size": 8, "strides": (4, 4), "padding": "valid",
+          "activation": "relu", "input_shape": (84, 84, 1, 32), "data_format":
+          "channels_first"}
+
+conv_2 = {"filters": 64, "kernel_size": 4, "strides": (2, 2), "padding": "valid",
+          "activation": "relu", "input_shape": (84, 84, 1, 32),
+          "data_format": "channels_first"}
+
+conv_3 = {"filters": 64, "kernel_size": 3, "strides": (1, 1), "padding": "valid",
+          "activation": "relu", "input_shape": (84, 84, 1, 32),
+          "data_format": "channels_first"}
+
+dense_1 = {"units": 512, "activation": "relu"}
+
+dense_2 = {"units": 6}
+
+compiler = {"loss": "mean_squared_error", "optimizer": RMSprop(lr=0.00025,
+                                                               rho=0.95,
+                                                               epsilon=0.01),
+            "metrics": ["accuracy"]}
+
+hp = {"cnn": [conv_1, conv_2, conv_3], "dense": [
+    dense_1, dense_2], "compiler": compiler}
+
+Conv2D( )
+
+cnn.set_model_params(hp)
